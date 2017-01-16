a21162befcdac3a7dabe544b9bb253810ee99f23
Petar <pbstojkov@gmail.com>
2017-01-17 00:29:23 +0100

    indentation fix

--- general/AvahiDiscovery.py
+++ general/AvahiDiscovery.py
@@ -7,11 +7,11 @@ def find_broker(room):
     # arg = ["avahi-browse", "-rta"]
     arg = ["avahi-browse", "-rt", "_room" + str(room) + "._sub._coap._udp"]
     while True:
-	    services = subprocess.check_output([arg[0], arg[1], arg[2]])
-	    # print(services)
-	    m = re.search('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', services)
-	    if m:
-	        found = m.group()
-	        return found
-	    time.sleep(3)
-	    # find_broker(room)
+        services = subprocess.check_output([arg[0], arg[1], arg[2]])
+        # print(services)
+        m = re.search('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', services)
+        if m:
+            found = m.group()
+            return found
+        time.sleep(3)
+        # find_broker(room)
