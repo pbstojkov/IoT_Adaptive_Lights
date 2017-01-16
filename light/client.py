ebf66dba97026a599a2d8cc160802292a9bd595a
Mendoza Marin <r.a.mendoza.marin@student.tue.nl>
2017-01-16 13:00:01 +0100

    Added lighting controller class

--- light/client.py
+++ light/client.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # from picamera import PiCamera
 from collections import deque
 from functools import reduce
+from general import WakaamaProc
 import math
 import argparse
 import time
