f9512f9102f9b5fde11b8df25354cb0426f8371d
Petar <pbstojkov@gmail.com>
2017-01-18 02:49:10 +0100

    wakaama should work now. parsing is different

--- general/WakaamaProc.py
+++ general/WakaamaProc.py
@@ -15,19 +15,38 @@ light_clientPath = "./light_lwm2mclient"
 INIT_RATE = 0.01
 
 #Search constants
-SEARCH_LIGHT_STATE = '[light_state]'
-SEARCH_SENSOR_STATE = '[sensor_state]'
-SEARCH_USER_TYPE = '[user_type]'
-SEARCH_USER_ID = '[user_id]'
-SEARCH_LIGHT_COLOR = '[light_color]'
-SEARCH_LOW_LIGHT = '[low_light]'
-SEARCH_GROUP_NO = '[group_no]'
-SEARCH_LOCATION_X = '[location_x]'
-SEARCH_LOCATION_Y = '[location_y]'
-SEARCH_ROOM_ID = '[room_id]'
-SEARCH_BEHAVIOR_DEPLOYMENT = '[behavior_deployment]'
-SEARCH_OWNERSHIP = '[ownership_priority]'
-SEARCH_LIGHT_BEHAVIOR = '[light_behavior]'
+# SEARCH_LIGHT_STATE = '[light_state]'
+# SEARCH_SENSOR_STATE = '[sensor_state]'
+# SEARCH_USER_TYPE = '[user_type]'
+# SEARCH_USER_ID = '[user_id]'
+# SEARCH_LIGHT_COLOR = '[light_color]'
+# SEARCH_LOW_LIGHT = '[low_light]'
+# SEARCH_GROUP_NO = '[group_no]'
+# SEARCH_LOCATION_X = '[location_x]'
+# SEARCH_LOCATION_Y = '[location_y]'
+# SEARCH_ROOM_ID = '[room_id]'
+# SEARCH_BEHAVIOR_DEPLOYMENT = '[behavior_deployment]'
+# SEARCH_OWNERSHIP = '[ownership_priority]'
+# SEARCH_LIGHT_BEHAVIOR = '[light_behavior]'
+SEARCH_LIGHT_STATE = "10250.0.2"
+SEARCH_USER_TYPE = "10250.0.3"
+SEARCH_USER_ID = "10250.0.4"
+SEARCH_LIGHT_COLOR = "10250.0.5"
+SEARCH_LOW_LIGHT = "10250.0.6"
+SEARCH_GROUP_NO = "10250.0.7"
+SEARCH_LOCATION_X = "10250.0.8"
+SEARCH_LOCATION_Y = "10250.0.9"
+SEARCH_ROOM_ID = "10250.0.10"
+SEARCH_BEHAVIOR_DEPLOYMENT = "10250.0.11"
+SEARCH_OWNERSHIP = "10250.0.12"
+SEARCH_LIGHT_BEHAVIOR = "10250.0.13"
+
+SEARCH_SENSOR_STATE = "10350.0.2"
+SEARCH_SENSOR_USER_ID = "10350.0.3"
+SEARCH_SENSOR_GROUP_NO = "10350.0.4"
+SEARCH_SENSOR_LOCATION_X = "10350.0.5"
+SEARCH_SENSOR_LOCATION_Y = "10350.0.6"
+SEARCH_SENSOR_ROOM_ID = "10350.0.7"
 
 #String definitions
 OCCUPIED = "OCCUPIED"
@@ -99,19 +118,19 @@ class Wakaama_Sensor():
 
     def __Read_Input_line(self, line):
         if SEARCH_SENSOR_STATE in line:
-            self.__sensor_state = line.split(',')[1]
+            self.__sensor_state = line.split("..")[-1] #line.split(',')[1]
 
-        elif SEARCH_ROOM_ID in line:
-            self.__room_id = line.split(',')[1]
+        elif SEARCH_SENSOR_ROOM_ID in line:
+            self.__room_id = line.split("..")[-1] #line.split(',')[1]
 
-        elif SEARCH_LOCATION_Y in line:
-            self.__location_y = float(line.split(',')[1])
+        elif SEARCH_SENSOR_LOCATION_Y in line:
+            self.__location_y = float(line.split("..")[-1]) #line.split(',')[1])
 
-        elif SEARCH_LOCATION_X in line:
-            self.__location_x = float(line.split(',')[1])
+        elif SEARCH_SENSOR_LOCATION_X in line:
+            self.__location_x = float(line.split("..")[-1]) #line.split(',')[1])
 
-        elif SEARCH_GROUP_NO in line:
-            self.__group_no = int(line.split(',')[1])
+        elif SEARCH_SENSOR_GROUP_NO in line:
+            self.__group_no = int(line.split("..")[-1]) #line.split(',')[1])
 
     def Set_Sensor_State(self, state):
         self.__sensor_state = state 
@@ -152,12 +171,53 @@ class Wakaama_Light:
         #todo: delete below line, it is just a test !
         self.__persons.load_json('https://iot-test.000webhostapp.com/OwnershipPriority.json')
 
+        self.__received_bytes_numb = 0
+        self.__accumulated_line = ""
+        self.__is_write_line = None
+
+    def __Parse_Part_Line(self, line):
+        print "Line: [" + line + "]"
+        if "bytes received" in line:
+            self.__received_bytes_numb = int(line.split(" ")[0])
+            print "Waiting for " + str(self.__received_bytes_numb) + " bytes."
+        elif self.__received_bytes_numb > 0:
+            if self.__is_write_line is None:
+                tmp = line.split("  ")[0].split(" ")[1]
+                if tmp == "03":
+                    self.__is_write_line = True
+                else:
+                    self.__is_write_line = False
+
+            data = line.split("  ")[-1][:-1]
+            if ' ' in data[:1]:
+                data = data.replace(' ', '')
+
+            self.__accumulated_line += data
+            print "Data: " + data
+            print "len: + " + str(len(data))
+            # print type(data)
+            try:
+                self.__received_bytes_numb -= len(data)
+            except Exception as e:
+                self.__received_bytes_numb = 0
+                return False
+            
+            if self.__received_bytes_numb == 0:
+                if self.__is_write_line:
+                    self.__is_write_line = None
+                    return True
+                else:
+                    self.__is_write_line = None
+                    return False
+                # return True
+        return False
+
     def __Start_Client_Process(self):
         # print type(self.__host)
         print self.__host
         
         try:
-            self.__cProc = subprocess.Popen([light_clientPath, "-4" , "-h", self.__host], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+            self.__cProc = subprocess.Popen([light_clientPath, "-4" , "-h", self.__host], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, universal_newlines=True)
         except:
             print "Fail"
             return False
@@ -191,7 +251,9 @@ class Wakaama_Light:
         for line in self.__WTF():
             # bs = self.__cProc.stdout.readline(100)
             # print "bs = {" + bs + "}"
-            self.__Read_Input_line(line)
+            if self.__Parse_Part_Line(line):
+                self.__Read_Input_line(self.__accumulated_line)
+                self.__accumulated_line = ""
 
         print("WTF!?!?!?")
         # self.__cProc.stdout.close()
@@ -204,15 +266,18 @@ class Wakaama_Light:
     def __Read_Input_line(self, line):
         print "reading .. [" + line + "]"
         if SEARCH_LIGHT_COLOR in line:
-            colors = line.split(',')
-            self.__light.change_color(colors[1][1:], colors[2], colors[3][:-1])
+            # colors = line.split(',')
+            colors_str = line.split("..")[-1]
+            colors = colors_str[1:-1].split(',')
+            self.__light.change_color(colors[0], colors[1], colors[2])
 
-            self.__light_color = colors[1] + ',' + colors[2] + ',' + colors[3]
+            self.__light_color = colors_str
             
             self.__persons.find_user_id_change_values(self.__user_id, light_color=self.__light_color)
 
         elif SEARCH_LOW_LIGHT in line:
-            ll = line.split(',')[1]
+            # ll = line.split(',')[1]
+            ll = line.split("..")[-1]
             if "True" in ll:
                 self.__low_light = True
             elif "False" in ll:
@@ -223,29 +288,30 @@ class Wakaama_Light:
             self.__persons.find_user_id_change_values(self.__user_id, low_light=self.__low_light)
 
         elif SEARCH_OWNERSHIP in line:
-            url_ownership = line.split(',')[1]
+            # url_ownership = line.split(',')[1]
+            url_ownership = line.split("..")[-1]
             self.__persons.load_json(url_ownership)
 
         elif SEARCH_ROOM_ID in line:
-            self.__room_id = line.split(',')[1]
+            self.__room_id = line.split("..")[-1] #line.split(',')[1]
 
         elif SEARCH_LOCATION_Y in line:
-            self.__location_y = float(line.split(',')[1])
+            self.__location_y = float(line.split("..")[-1]) #line.split(',')[1])
 
         elif SEARCH_LOCATION_X in line:
-            self.__location_x = float(line.split(',')[1])
+            self.__location_x = float(line.split("..")[-1]) #line.split(',')[1])
 
         elif SEARCH_GROUP_NO in line:
-            self.__group_no = int(line.split(',')[1])
+            self.__group_no = int(line.split("..")[-1]) #line.split(',')[1])
 
         elif SEARCH_LIGHT_STATE in line:
-            self.__light_state = line.split(',')[1]
+            self.__light_state = line.split("..")[-1] #line.split(',')[1]
 
         elif SEARCH_USER_TYPE in line:
-            self.__user_type = line.split(',')[1]
+            self.__user_type = line.split("..")[-1] #line.split(',')[1]
 
         elif SEARCH_USER_ID in line:
-            self.__user_id = line.split(',')[1]
+            self.__user_id = line.split("..")[-1] #line.split(',')[1]
 
 
     def Free_Sensor_State(self):
@@ -285,6 +351,9 @@ class Wakaama_Light:
     def Kill_Client_Process(self):
         output = subprocess.check_output(["pkill","light_lwm2mclient"])
         print output
+
+    def get_persons(self):
+        return self.__persons
 
 
 if __name__ == "__main__":
